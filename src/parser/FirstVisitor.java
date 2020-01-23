a949928585538110c8a2c9c17a1cbf2909849d93
Yannis <admin@coding-hard.com>
2020-01-23 23:38:46 +0100

    implemented FirstVisitor methods

--- src/parser/FirstVisitor.java
+++ src/parser/FirstVisitor.java
@@ -12,6 +12,21 @@ public class FirstVisitor implements Visitor {
 	@Override
 	public void visit(OperandNode node) {
 		
+		switch (node.symbol) {
+			case "epsilon":
+				node.nullable = true;
+				break;
+			default:
+				node.nullable = false;
+				break;
+		}
+		
+		node.position = positionCounter;
+		positionCounter++;
+		
+		node.firstpos.add(node.position);
+		
+		node.lastpos.add(node.position);
 	}
 
 	@Override
@@ -21,8 +36,30 @@ public class FirstVisitor implements Visitor {
 
 		switch (node.operator) {
 			case "|":
+				node.nullable = leftNode.nullable || rightNode.nullable;
+				
+				node.firstpos.addAll(leftNode.firstpos);
+				node.firstpos.addAll(rightNode.firstpos);
+				
+				node.lastpos.addAll(leftNode.lastpos);
+				node.lastpos.addAll(rightNode.lastpos);
 				break;
 			case "*":
+				node.nullable = leftNode.nullable && rightNode.nullable;
+				
+				if (leftNode.nullable) {
+					node.firstpos.addAll(leftNode.firstpos);
+					node.firstpos.addAll(rightNode.firstpos);
+				} else {
+					node.firstpos.addAll(leftNode.firstpos);
+				}
+				
+				if (rightNode.nullable) {
+					node.lastpos.addAll(leftNode.lastpos);
+					node.lastpos.addAll(rightNode.lastpos);
+				} else {
+					node.lastpos.addAll(rightNode.lastpos);
+				}
 				break;
 			default:
 				throw new RuntimeException("Wrong Operator Symbol");
@@ -35,10 +72,25 @@ public class FirstVisitor implements Visitor {
 		
 		switch (node.operator) {
 			case "*":
+				node.nullable = true;
+				
+				node.firstpos.addAll(subNode.firstpos);
+				
+				node.lastpos.addAll(subNode.lastpos);
 				break;
 			case "+":
+				node.nullable = subNode.nullable;
+				
+				node.firstpos.addAll(subNode.firstpos);
+				
+				node.lastpos.addAll(subNode.lastpos);
 				break;
 			case "?":
+				node.nullable = true;
+				
+				node.firstpos.addAll(subNode.firstpos);
+				
+				node.lastpos.addAll(subNode.lastpos);
 				break;
 			default:
 				throw new RuntimeException("Wrong Unary Symbol");
