6c710553fcf92443748662982fe8e28822523c70
Yannis <admin@coding-hard.com>
2020-01-24 14:10:04 +0100

    added junit test for SecondVisitorTests

---
+++ src/test/SecondVisitorTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SecondVisitorTests {
+	
+	/*
+	 * JUnit 5 in Eclipse
+	 * ParserTests
+	 * 
+	 */
+
+	@Test
+	@DisplayName("Test ParserTests")
+	void test() {
+		
+		
+	}
+	
+}
