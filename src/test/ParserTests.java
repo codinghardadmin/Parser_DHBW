33d52ef1edb666590a165293c4528b5c66c3ed87
Yannis <admin@coding-hard.com>
2020-01-24 14:09:44 +0100

    added junit test for ParserTests

---
+++ src/test/ParserTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParserTests {
+	
+	/*
+	 * JUnit 5 in Eclipse
+	 * SecondVisitorTests
+	 * 
+	 */
+
+	@Test
+	@DisplayName("Test SecondVisitorTests")
+	void test() {
+		
+		
+	}
+	
+}
