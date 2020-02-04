dbb8fee94d25300aa3dce9ef02983dc6a8be64b3
Yannis <admin@coding-hard.com>
2020-02-04 21:38:25 +0100

    implemented SecondVisitor

--- src/visitors/SecondVisitor.java
+++ src/visitors/SecondVisitor.java
@@ -1,6 +1,7 @@
 package visitors;
 
-import java.util.ArrayList;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import nodes.BinOpNode;
 import nodes.OperandNode;
@@ -9,25 +10,26 @@ import nodes.UnaryOpNode;
 
 public class SecondVisitor implements Visitor {
 	
-	// Evtl. HashMap
-	private ArrayList<FollowposTableEntry> table;
+	public SortedMap<Integer, FollowposTableEntry> followposTableEntries = new TreeMap<>();
 	
 	public SecondVisitor(Visitable root, int positions) {
 		DepthFirstIterator.traverse(root, this);
-		table = new ArrayList<FollowposTableEntry>();
 	}
 	
 	@Override
 	public void visit(OperandNode node) {
-		
+		followposTableEntries.put(node.position, new FollowposTableEntry(node.position, node.symbol));
 	}
 
 	@Override
 	public void visit(BinOpNode node) {
+		SyntaxNode leftNode = (SyntaxNode)node.left;
+		SyntaxNode rightNode = (SyntaxNode)node.right;
+		System.out.println(node.operator);
 		switch (node.operator) {
-		case "*":
-			for (int nodepos : node.lastpos) {
-				
+		case "°":
+			for (int nodepos : leftNode.lastpos) {
+				followposTableEntries.get(nodepos).followpos.addAll(rightNode.firstpos);
 			}
 			break;
 		case "+":
@@ -35,20 +37,24 @@ public class SecondVisitor implements Visitor {
 			break;
 
 		default:
-			throw new RuntimeException("Wrong Unary Symbol");
+			//throw new RuntimeException("Wrong Unary Symbol");
 	}
 	}
 
 	@Override
 	public void visit(UnaryOpNode node) {
-		// SyntaxNode subNode = (SyntaxNode)node.subNode;
+		SyntaxNode subNode = (SyntaxNode)node.subNode;
 		
 		switch (node.operator) {
 			case "*":
-				
+				for (int nodepos : subNode.lastpos) {
+					followposTableEntries.get(nodepos).followpos.addAll(subNode.firstpos);
+				}
 				break;
 			case "+":
-				
+				for (int nodepos : subNode.lastpos) {
+					followposTableEntries.get(nodepos).followpos.addAll(subNode.firstpos);
+				}
 				break;
 
 			default:
